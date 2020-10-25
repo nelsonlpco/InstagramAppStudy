799cd2d0709ff9819b52e49d208909175e91af13
Nelson <nelsonlpco@gmail.com>
2020-10-25 18:27:06 -0300

    added login screen layout

--- app/src/main/java/com/example/instagramappstudy/login/presentation/MainActivity.kt
+++ app/src/main/java/com/example/instagramappstudy/login/presentation/MainActivity.kt
@@ -1,11 +1,12 @@
-package com.example.instagramappstudy
+package com.example.instagramappstudy.login.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.instagramappstudy.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
     }
 }
